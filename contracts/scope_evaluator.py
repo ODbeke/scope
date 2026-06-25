36043b913a21f466f0ff0b56d4036aeb0d6c8d52
ODbeke <okoyeprosper475@gmail.com>
2026-06-25 06:49:41 +0100

    Add gltest configuration and initial contract skeleton

---
+++ contracts/scope_evaluator.py
@@ -0,0 +1,9 @@
+# { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
+from genlayer import *
+
+class ScopeEvaluator(gl.Contract):
+    """
+    SCOPE: Smart Contract Outcome & Performance Evaluator.
+    Evaluates milestone completion based on scope of work and outcomes.
+    """
+    pass
